bb60c8cdaef6fdf13bf045dc319d573842415dfd
Adam Płaczek <adam.placzek@gmail.com>
2019-09-25 14:59:42 +0200

    Update config-jenkins.sh

--- jenkins-tutorial/config-jenkins.sh
+++ jenkins-tutorial/config-jenkins.sh
@@ -6,7 +6,7 @@ sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sour
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install oracle-java8-installer -y
+sudo apt-get install openjdk-8-jre-headlessr -y
 sudo apt-get install jenkins -y
 
 # Docker
